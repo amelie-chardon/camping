44de3a5964c0cfe35f8425074b5a5514a60d33ce
colas <jean-davidson.colas@laplateforme.io>
2020-03-03 22:23:41 +0100

    change branche

--- admin.php
+++ admin.php
@@ -39,6 +39,94 @@ if($_SESSION['user']->getrole() != "admin"){
 </section>
 
 
+<?php
+$_SESSION['bdd']->connect();
+$price=$_SESSION['bdd']->execute("SELECT prix,nom FROM prix");
+//var_dump($price);
+$resa=$_SESSION['bdd']->execute("SELECT reservations.debut,reservations.fin,reservations.id,utilisateurs.login  FROM reservations INNER JOIN utilisateurs ON reservations.id_utilisateur = utilisateurs.id");
+//var_dump($resa);
+$_SESSION['bdd']->close();
+
+foreach($resa as $resat) 
+{ 
+     
+?>
+<table>
+<thead>
+    <tr>
+        <th>Date de debut </th>
+        <th>Date de fin</th>
+        <th>Prix</th>
+        <th>Client</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+    <td><?php echo $resat[0] ; ?> </td>
+    <td><?php echo $resat[1] ; ?> </td>
+    <td><?php echo $resat[2] ; ?> </td>
+    <td><?php echo $resat[3] ; ?> </td>
+    <td>
+    <form method="post" action="admin.php" id="suppression">
+    <button type="submit" id="submit" name="resat" value ="<?php echo $resat[2];?>">Supprimer</button></form>
+    </td>
+    </tr>
+</tbody>
+</table>
+<?php
+//*suppression de l'article 
+if(isset($_POST['resat']))
+{
+    $_SESSION['bdd']->delete();
+    header('Location:admin.php');
+    //$_SESSION['bdd']->execute("DELETE reservations.debut,reservations.fin FROM reservations WHERE `reservations`.`id` = 28");
+    //$suppr = $_SESSION['bdd']->execute("DELETE FROM reservations WHERE id = '".$_POST['resat']."'");
+    //var_dump($suppr);
+}
+
+}
+$_SESSION['bdd']->connect();
+$price=$_SESSION['bdd']->execute("SELECT prix,nom,id FROM prix");
+foreach($price as $price3) 
+{ 
+     
+?>
+<table>
+<thead>
+    <tr>
+        <th>Type</th>
+        <th>Prix</th>
+        <th></th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+    <td><?php echo $price3[1] ; ?> </td>
+    <td><?php echo $price3[0] ; ?> </td>
+    <td> </td>
+    <td>
+    <form method="post" action="admin.php" id="modifier">
+    <input type="text" name="<?php echo $price3[2] ;?>" value =""></td>
+    <td>
+    <input type="submit" id="submit" name="price<?php echo $price3[2];?>" value="Modifier"/>
+    </form>
+    </td>
+    </tr>
+</tbody>
+  </table>  
+<?php
+//Modifier les prix 
+
+if(isset($_POST['price'.$price3[2]]))
+{
+    $_SESSION['bdd']->update($price3[2]);
+    header('Location:admin.php');
+    
+    
+}
+
+}
+    
 ?>
 
 </main>
--- class/bdd.php
+++ class/bdd.php
@@ -1,16 +1,19 @@
 <?php
 
-class bdd{
+class bdd
+{
 
     private $query="";
     protected $connexion = "";
     private $result=[];
 
 
-    public function connect(){
+    public function connect()
+    {
         $connect = mysqli_connect('localhost', 'root', '','camping');
         //var_dump($connect);
-        if($connect == false){
+        if($connect == false)
+        {
             return false;
         }
         $this->connexion = $connect;
@@ -23,6 +26,7 @@ class bdd{
 
 
     public function execute($query)
+    { 
         {
             $this->query=$query;
             $execute=mysqli_query($this->connexion, $query);
@@ -41,4 +45,30 @@ class bdd{
             return $this->result;
         }
     }
+
+    public function delete()
+    {
+        
+        if (!isset($_SESSION))
+        {
+           echo "Impossible de supprimer. Veuillez d'abord vous connecter.";
+        }
+        else
+        {
+            $connect = mysqli_connect("localhost", "root", "", "camping");
+            $delete="DELETE FROM reservations WHERE id = '".$_POST['resat']."'";
+            $query=mysqli_query($connect,$delete);
+            unset($_SESSION['login']);
+            echo "La réservation a bien été supprimer";
+        }
+    }
+
+    public function update($id)
+    {
+        $connect = mysqli_connect("localhost", "root", "", "camping");
+        $update="UPDATE prix SET prix = ".$_POST[$id]." WHERE id = $id";
+        $query_update=mysqli_query($connect,$update);
+        echo "le prix a bien été modifier.";
+    }
+}    
 ?>
